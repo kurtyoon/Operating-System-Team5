42367c698033680560e8a933bb9f4a240c3f4af9
장 재 훈 <peng2412@naver.com>
2023-05-22 18:19:16 +0900

    comment directoryTree
    comment directoryTree

--- source/directorytree.c
+++ source/directorytree.c
@@ -1,6 +1,8 @@
 #include "../include/main.h"
 
-
+//loadDirectoryTree
+//"file.Directory,txt", read each line and making new Node, and DirectoryTree에 추가
+//fclose, 로드된 디렉토리의 트리의 현재 위치를 root로 설정 후, dirTree 반환.
 DirectoryTree *loadDirectoryTree() {
     DirectoryTree *dirTree = (DirectoryTree *)malloc(sizeof(DirectoryTree));
     char tmp[MAX_LENGTH];
@@ -14,6 +16,7 @@ DirectoryTree *loadDirectoryTree() {
     return dirTree;
 }
 
+//text file로 save.
 void saveDirectoryTree(DirectoryTree *dirTree, Stack *dirStack) {
     Dir = fopen("file/Directory.txt", "w");
     writeDirNode(dirTree, dirTree->root, dirStack);
