5bb17746387621dd45e4470f0c99e384729d21a1
kurtyoon <akakertcobain@naver.com>
2023-06-04 21:29:35 +0900

    Refactor rm

--- source/rm.c
+++ source/rm.c
@@ -1,11 +1,9 @@
 #include "../include/main.h"
 
-// free directory node
 void destroyNode(DirectoryNode *dirNode) {
     free(dirNode);
 }
 
-// if directory node has child or sibling, use recursive
 void destroyDir(DirectoryNode *dirNode) {
     if (dirNode->nextSibling) {
         destroyDir(dirNode->nextSibling);
@@ -13,46 +11,47 @@ void destroyDir(DirectoryNode *dirNode) {
     if (dirNode->firstChild) {
         destroyDir(dirNode->firstChild);
     }
-    dirNode->firstChild = NULL;
-    dirNode->nextSibling = NULL;
     destroyNode(dirNode);
 }
 
-int removeDir(DirectoryTree *dirTree, char *dirName) {
-    DirectoryNode *DelNode = NULL;
-    DirectoryNode *tmpNode = NULL;
+DirectoryNode *findAndDestroyDir(DirectoryTree *dirTree, char *dirName) {
+    DirectoryNode *delNode = NULL;
+    DirectoryNode *tmpNode = dirTree->current->firstChild;
     DirectoryNode *prevNode = NULL;
 
-    tmpNode = dirTree->current->firstChild;
-    if (!tmpNode) {
+    while (tmpNode && strcmp(tmpNode->name, dirName)) {
+        prevNode = tmpNode;
+        tmpNode = tmpNode->nextSibling;
+    }
+
+    if (tmpNode) {
+        delNode = tmpNode;
+        if (prevNode) {
+            prevNode->nextSibling = delNode->nextSibling;
+        } else {
+            dirTree->current->firstChild = delNode->nextSibling;
+        }
+        if (delNode->firstChild) destroyDir(delNode->firstChild);
+        destroyNode(delNode);
+    } else {
         printf("rm: Can not remove '%s': No such file or directory.\n", dirName);
+        return NULL;
+    }
+
+    return delNode;
+}
+
+int removeDir(DirectoryTree *dirTree, char *dirName) {
+    if (!findAndDestroyDir(dirTree, dirName)) {
         return FAIL;
     }
-    // if tmpNode->name == dirName
-    if(!strcmp(tmpNode->name, dirName)) {
-        dirTree->current->firstChild = tmpNode->nextSibling;
-        DelNode = tmpNode;
-        if (DelNode->firstChild) destroyDir(DelNode->firstChild);
-        destroyNode(DelNode);
-    } else {
-        while (tmpNode) {
-            if (!strcmp(tmpNode->name, dirName)) {
-                DelNode = tmpNode;
-                break;
-            }
-            prevNode = tmpNode;
-            tmpNode = tmpNode->nextSibling;
-        }
-        if (DelNode) {
-            prevNode->nextSibling = DelNode->nextSibling;
-            if (DelNode->firstChild) destroyDir(DelNode->firstChild);
-            destroyNode(DelNode);
-        } else {
-            printf("rm: Can not remove '%s': No such file or directory.\n", dirName);
-            return FAIL;
-        }
-    }
     return SUCCESS;
+}
+
+void executeCommand(DirectoryTree *dirTree, ThreadTree *threadTree, int option, char *command) {
+    threadTree->threadTree = dirTree;
+    threadTree->command = command;
+    threadTree->option = option;
 }
 
 int ft_rm(DirectoryTree *dirTree, char *command) {
@@ -86,9 +85,7 @@ int ft_rm(DirectoryTree *dirTree, char *command) {
                 return FAIL;
             }
             while (str) {
-                threadTree[threadCount].threadTree = dirTree;
-                threadTree[threadCount].command = str;
-                threadTree[threadCount++].option = 1;
+                executeCommand(dirTree, &threadTree[threadCount++], 1, str);
                 str = strtok(NULL, " ");
             }
         } else if(!strcmp(command, "-f")) {
@@ -99,9 +96,7 @@ int ft_rm(DirectoryTree *dirTree, char *command) {
                 return FAIL;
             }
             while (str) {
-                threadTree[threadCount].threadTree = dirTree;
-                threadTree[threadCount].option = 2;
-                threadTree[threadCount++].command = str;
+                executeCommand(dirTree, &threadTree[threadCount++], 2, str);
                 str = strtok(NULL, " ");
             }
         } else if (!strcmp(command, "-rf") || !strcmp(command, "-fr")) {
@@ -110,9 +105,7 @@ int ft_rm(DirectoryTree *dirTree, char *command) {
                 return FAIL;
             }
             while (str) {
-                threadTree[threadCount].threadTree = dirTree;
-                threadTree[threadCount].option = 3;
-                threadTree[threadCount++].command = str;
+                executeCommand(dirTree, &threadTree[threadCount++], 3, str);
                 str = strtok(NULL, " ");
             }
         } else if (!strcmp(command, "--help")) {
@@ -137,18 +130,14 @@ int ft_rm(DirectoryTree *dirTree, char *command) {
         }
     } else {
         str = strtok(NULL, " ");
-        threadTree[threadCount].threadTree = dirTree;
-        threadTree[threadCount].option = 0;
-        threadTree[threadCount++].command = command;
+        executeCommand(dirTree, &threadTree[threadCount++], 0, command);
         if (!command) {
             printf("rm: Invalid option\n");
             printf("Try 'rm --help' for more information.\n");
             return FAIL;
         }
         while (str) {
-            threadTree[threadCount].threadTree = dirTree;
-            threadTree[threadCount].option = 0;
-            threadTree[threadCount++].command = str;
+            executeCommand(dirTree, &threadTree[threadCount++], 0, str);
             str = strtok(NULL, " ");
         }
     }
