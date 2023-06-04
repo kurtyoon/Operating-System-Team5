04f1ca8e6c8fc6979633c476de4eb0b5a2488199
kurtyoon <akakertcobain@naver.com>
2023-06-04 18:21:15 +0900

    Refactor cp

--- source/cp.c
+++ source/cp.c
@@ -1,22 +1,49 @@
 #include "../include/main.h"
 
-int ft_cp(DirectoryTree* dirTree, char* SourceName, char* ObjectName) {
-    char buf[1024];
-    int original, new;
-    int nread;
+#define BUF_SIZE 1024
 
-    if (access(SourceName, F_OK)) {
-        printf("Not Exist Original File.\n");
+int ft_cp(DirectoryTree* dirTree, char* srcName, char* dstName) {
+    char buf[BUF_SIZE];
+    int srcFile, dstFile;
+    ssize_t nread;
+
+    if (access(srcName, F_OK)) {
+        printf("Source file does not exist.\n");
         return FAIL;
     }
-    if (!strcmp(SourceName, ObjectName)) {
-        printf("cp: '%s' and '%s' are the same file.\n", SourceName, ObjectName);
+    if (!strcmp(srcName, dstName)) {
+        printf("cp: '%s' and '%s' are the same file.\n", srcName, dstName);
         return FAIL;
     }
-    original = open(SourceName, O_RDONLY);
-    new = open(ObjectName, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
-    nread = read(original, buf, sizeof(buf));
-    write(new, buf, nread);
-    makeDir(dirTree, ObjectName, 'f');
+
+    srcFile = open(srcName, O_RDONLY);
+    if (srcFile == FAIL) {
+        perror("Unable to open source file");
+        return FAIL;
+    }
+    dstFile = open(dstName, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+    if (dstFile == FAIL) {
+        perror("Unable to open destination file");
+        close(srcFile);
+        return FAIL;
+    }
+
+    while ((nread = read(srcFile, buf, BUF_SIZE)) > 0) {
+        if (write(dstFile, buf, nread) != nread) {
+            perror("Error writing to destination file");
+            close(srcFile);
+            close(dstFile);
+            return FAIL;
+        }
+    }
+
+    if (nread == FAIL) {
+        perror("Error reading source file");
+    }
+
+    close(srcFile);
+    close(dstFile);
+    makeDir(dirTree, dstName, 'f');
+
     return SUCCESS;
 }
